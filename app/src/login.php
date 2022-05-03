e923a7b1b5ff0100fee2be0ce64df67b75b16eb7
Marcos Felipe <mfelipe@worcester.edu>
2022-05-03 01:28:55 -0400

    fix minor error

--- app/src/login.php
+++ app/src/login.php
@@ -10,67 +10,67 @@ try {
     if($myusername==null || $mypassword==null)
     {$error = true;}
 
-    if(!filter_var($mypassword, FILTER_VALIDATE_EMAIL) || !(preg_match("^[a-zA-Z0-9]{5,20}$", $mypassword)))
+    if(!filter_var($myusername, FILTER_VALIDATE_EMAIL) || !(preg_match("^[a-zA-Z0-9]{5,20}$", $mypassword)))
     {$error = true;}
 
     if (!$error){
         //convert password to 80 byte hash using ripemd256 before comparing
-    $hashpassword = hash('ripemd256', $mypassword);
+        $hashpassword = hash('ripemd256', $mypassword);
 
-    $myusername = strtolower($myusername); //makes username noncase-sensitive
-    global $acctype;
+        $myusername = strtolower($myusername); //makes username noncase-sensitive
+        global $acctype;
 
 
-    //query for count
-    $query = "SELECT COUNT(*) as count FROM User WHERE Email='$myusername' AND (Password='$mypassword' OR Password='$hashpassword')";
-    $count = $db->querySingle($query);
+        //query for count
+        $query = "SELECT COUNT(*) as count FROM User WHERE Email='$myusername' AND (Password='$mypassword' OR Password='$hashpassword')";
+        $count = $db->querySingle($query);
 
-    //query for the row(s)
-    $query = "SELECT * FROM User WHERE Email='$myusername' AND (Password='$mypassword' OR Password='$hashpassword')";
-    $results = $db->query($query);
+        //query for the row(s)
+        $query = "SELECT * FROM User WHERE Email='$myusername' AND (Password='$mypassword' OR Password='$hashpassword')";
+        $results = $db->query($query);
 
-    if ($results !== false) //query failed check
-    {
-        if (($userinfo = $results->fetchArray()) !== (null || false)) //checks if rows exist
+        if ($results !== false) //query failed check
         {
-            // users or user found
-            $error = false;
+            if (($userinfo = $results->fetchArray()) !== (null || false)) //checks if rows exist
+            {
+                // users or user found
+                $error = false;
 
-            $acctype = $userinfo[2];
+                $acctype = $userinfo[2];
+            } else {
+                // user was not found
+                $error = true;
+
+            }
         } else {
-            // user was not found
+            //query failed
             $error = true;
 
         }
-    } else {
-        //query failed
-        $error = true;
 
-    }
+        //determine if an account that met the credentials was found
+        if ($count >= 1 && !$error) {
+            //login success
 
-    //determine if an account that met the credentials was found
-    if ($count >= 1 && !$error) {
-        //login success
-
-        if (isset($_SESSION)) {
-            //a session already existed
-            session_destroy();
-            session_start();
-            $_SESSION['email'] = $myusername;
-            $_SESSION['acctype'] = $acctype;
+            if (isset($_SESSION)) {
+                //a session already existed
+                session_destroy();
+                session_start();
+                $_SESSION['email'] = $myusername;
+                $_SESSION['acctype'] = $acctype;
+            } else {
+                //a session did not exist
+                session_start();
+                $_SESSION['email'] = $myusername;
+                $_SESSION['acctype'] = $acctype;
+            }
+            //redirect
+            header("Location: ../public/dashboard.php");
         } else {
-            //a session did not exist
-            session_start();
-            $_SESSION['email'] = $myusername;
-            $_SESSION['acctype'] = $acctype;
+            //login fail
+            header("Location: ../public/index.php?login=fail");
         }
-        //redirect
-        header("Location: ../public/dashboard.php");
-    } else {
-        //login fail
-        header("Location: ../public/index.php?login=fail");
-    }
-//note: since the database is not changed, it is not backed up
+        //note: since the database is not changed, it is not backed up
     }else{
         //login fail
         header("Location: ../public/index.php?login=fail");
